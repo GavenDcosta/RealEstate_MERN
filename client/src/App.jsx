f870232cd792d06f886eca1df4c6ca43f434b206
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-04 12:28:01 +0530

    routes and router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
-import Navbar from "./components/navbar/Navbar"
-import "./layout.scss"
 import HomePage from "./routes/homePage/HomePage"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/ListPage"
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/SinglePage";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path:"/",
+          element: <HomePage />,
+        },
+        {
+          path:"/list",
+          element: <ListPage />,
+        },
+        {
+          path:"/:id",
+          element: <SinglePage />,
+        }
+      ]
+    },
+  ]);
+  
   return (
-    <div className="layout">
-
-      <div className="navbar">
-        <Navbar />
-      </div>
-
-      <div className="content">
-        <HomePage />
-      </div>
-      
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
