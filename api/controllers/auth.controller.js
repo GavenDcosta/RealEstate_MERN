7399b15db5e89f0bd90979a1183d0eb2941a5bec
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-07 11:32:56 +0530

    backend authentication routes done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,87 @@
-export const register = (req, res) => {
-    //db operations 
-    console.log("register")
-}
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
+import jwt from "jsonwebtoken"
+import 'dotenv/config';
 
-export const login = (req, res) => {
-    //db operations 
+
+export const register = async (req, res) => {
+
+    try{
+        const { username, email, password } = req.body
+
+        //hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+    
+        console.log(hashedPassword)
+    
+        // create new user and save 
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password:hashedPassword,
+            },
+        })
+    
+        console.log(newUser)
+    
+        res.status(201).json({ message: "User created successfully" })
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: "Failed to Create User" })
+    }
+    
+}
+    
+
+export const login = async (req, res) => {
+    try{
+        const { username, password }  = req.body
+
+        //check if user exists
+        const user = await prisma.user.findUnique({
+            where:{username}
+            // where:{username:username}
+        })
+
+        if(!user){
+            res.status(401).json({message: "Invalid Credentials!"})
+        }
+    
+        //check if password is correct 
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid) return res.status(401).json({message: "Invalid Credentials!"})
+        
+    
+        //generate cookie token and send to the user 
+
+        // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+
+        const age = 1000 * 60 * 60 * 24 * 7  //1 week
+
+        const token = jwt.sign({
+            id:user.id
+        }, 
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: age
+        }
+        )
+
+        res.cookie("token", token, {
+            httpOnly:true,
+            //secure:true
+            maxAge:age,
+        }).status(200).json({message:"User Logged In Successfully"})
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: "Failed to Login" })
+    }
+
 }
 
 export const logout = (req, res) => {
-    //db operations 
+    res.clearCookie("token").status(200).json({message:"User Logged Out Successfully"})
 }
