577ff39c177c683475a125f8c8e125f6bc82473a
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-08 07:22:19 +0530

    upload images for profile done

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -3,13 +3,15 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
 import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
 
 function ProfileUpdatePage() {
+  const {updateUser, currentUser} = useContext(AuthContext)
 
   const [error, setError] = useState("")
+  const [avatar, setAvatar] = useState(currentUser.avatar)
   const [isLoading, setIsLoading] = useState(false)
 
-  const {updateUser, currentUser} = useContext(AuthContext)
 
   const navigate = useNavigate()
 
@@ -23,7 +25,7 @@ function ProfileUpdatePage() {
 
      try{
         const res = await apiRequest.put(`/users/${currentUser.id}`, {
-          username, email, password
+          username, email, password, avatar
         })
 
         updateUser(res.data)
@@ -70,7 +72,16 @@ function ProfileUpdatePage() {
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser.avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <img src={avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <UploadWidget uwConfig={{
+          cloudName: "dmhr3fumd",
+          uploadPreset: "mgrealestate",
+          multiple: false,
+          maxImageFileSize: 2000000,
+          folder: "avatars"
+         }}
+         setAvatar={setAvatar}
+        />
       </div>
     </div>
   );
