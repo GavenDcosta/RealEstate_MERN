16413280b5be3f43984ffca4bfd0334e9477d564
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-09 14:39:52 +0530

    integrated chat feature in UI

--- client/src/routes/profilePage/ProfilePage.jsx
+++ client/src/routes/profilePage/ProfilePage.jsx
@@ -93,7 +93,16 @@ const ProfilePage = () => {
         </div>
         <div className="chatContainer">
             <div className="wrapper">
-                 <Chat/>
+                  <Suspense fallback={<p>Loading...</p>}>
+                    <Await
+                      resolve={data.chatResponse}
+                      errorElement={
+                        <p>Error loading Chats!</p>
+                      }
+                    >
+                      {(chatResponse) =>  <Chat chats={chatResponse.data} />}
+                    </Await>
+                 </Suspense>
             </div>
         </div>
     </div>
